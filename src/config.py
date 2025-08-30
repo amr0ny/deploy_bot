dab0073f5484fdcbfebb2592a54f6781c7e193c7
Alex <38862440+amr0ny@users.noreply.github.com>
2025-08-30 21:16:38 +0300

    fix: small fixes

--- src/config.py
+++ src/config.py
@@ -10,9 +10,9 @@ class AppConfig(BaseSettings):
     debug: bool = False
     queue_name: str = "default"
     queue_maxsize: int = 10000
-    facts_dir_path = "../facts"
-    short_facts_file = "short_facts.txt"
-    medium_facts_file = "medium_facts.txt"
+    facts_dir_path: str = "../facts"
+    short_facts_file: str = "short_facts.txt"
+    medium_facts_file: str = "medium_facts.txt"
 
     admin_ids: List[int] = []
 
